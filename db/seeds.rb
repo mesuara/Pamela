66bd2ac41718242e78609cc9bdd10b424033027e
mesuara <mesuara@gmail.com>
2018-08-27 14:18:17 -0400

    added devise

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,9 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-Admin.create(name: 'test', last_name: 'test', email: 'test@test.com', password: '123')
+# Admin.create(name: 'test', last_name: 'test', email: 'test@test.com', password: '123')
 
-Course.create(name:'SEI', total_hours: 480)
-Instructor.create(name: 'David', last_name: 'Patlut', age: 100, salary: 1, education: "Bachelor Degree", email: "db@email.com", password: "123")
-Cohort.create(name: 'Summer', start_date: DateTime.new(2018,6,18), end_date: DateTime.new(2018,9,26), instructor_id: 1, course_id: 1)
-Student.create(name: 'Lily', last_name: 'Ngo', age: 20, email: 'lily@gmail.com', password: '321', cohort_id: 1)
+# Course.create(name:'SEI', total_hours: 480)
+# Instructor.create(name: 'David', last_name: 'Patlut', age: 100, salary: 1, education: "Bachelor Degree", email: "db@email.com", password: "123")
+# Cohort.create(name: 'Summer', start_date: DateTime.new(2018,6,18), end_date: DateTime.new(2018,9,26), instructor_id: 1, course_id: 1)
+# Student.create(name: 'Lily', last_name: 'Ngo', age: 20, email: 'lily@gmail.com', password: '321', cohort_id: 1)
+
+
+   User.create(email: "test@gmail.com", encrypted_password:"123456" )
