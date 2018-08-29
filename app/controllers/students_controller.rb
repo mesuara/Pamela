92af66b1a028817e52b59835d6a011ab1081b0c4
mesuara <mesuara@gmail.com>
2018-08-29 09:16:49 -0400

    instrutos can see his own students when logged in

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -9,9 +9,11 @@ class StudentsController < ApplicationController
         @student = Student.find_by(user_id: @user_id)
         @cohort = Cohort.find(@student.cohort_id)
         pp @student
-      else
+      elsif current_user && current_user.instructor
         @instructor = Instructor.find_by(user_id: @current_user.id)
+        pp @instructor
         @cohort = Cohort.find_by(instructor_id: @instructor.id)
+        pp @cohort
         @allstudent = Student.all
         @student= []
         @allstudent.each do |student|
@@ -42,7 +44,7 @@ class StudentsController < ApplicationController
         if current_user && current_user.admin
           @student_user = User.create(email: params[:student][:email], password: params[:student][:password], student: true)
           @id = @student_user.id
-       @student=  Student.create(name: params[:student][:name], last_name: params[:student][:last_name],age: params[:student][:age], email: params[:student][:email],password: params[:student][:password], cohort_id: params[:student][:cohort_id], user_id: @id)
+       @student =  Student.create(name: params[:student][:name], last_name: params[:student][:last_name], age: params[:student][:age], email: params[:student][:email], password: params[:student][:password], cohort_id: params[:student][:cohort_id], user_id: @id)
        p "i am create"
         redirect_to '/students'
       else 
